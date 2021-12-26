da9ba3f09c57bfe8cfd7f1268ba8fe4238a27662
zerofille <nstrdnv@gmai.com>
2021-12-26 15:21:36 +0200

    Сhange button's rendering

--- Client/src/Components/VoteBtns/VoteBtns.jsx
+++ Client/src/Components/VoteBtns/VoteBtns.jsx
@@ -3,7 +3,7 @@ import "./VoteBtns.css";
 import { createVoteRequest } from "../../app/votesSlice";
 import { useDispatch } from "react-redux";
 import { getVoteRequest } from "../../app/votesSlice";
-
+import { initialState } from "../../app/votesSlice";
 function VoteBtns({ setModalActive, date }) {
   const dispatch = useDispatch();
   const closeWindow = (n) => {
@@ -13,37 +13,22 @@ function VoteBtns({ setModalActive, date }) {
       dispatch(getVoteRequest(date))
     );
   };
+
+  console.log(initialState.data);
   return (
     <div className="wrapper">
       <h1 className="text">Vote for your favorite number!</h1>
       <div className="wrapperBtn">
-        <button className="numberBtn" onClick={() => closeWindow(1)}>
-          1
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(2)}>
-          2
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(3)}>
-          3
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(4)}>
-          4
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(5)}>
-          5
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(6)}>
-          6
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(7)}>
-          7
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(8)}>
-          8
-        </button>
-        <button className="numberBtn" onClick={() => closeWindow(9)}>
-          9
-        </button>
+        {initialState.data.map((i) => {
+          return (
+            <button
+              className="numberBtn"
+              onClick={() => closeWindow(i.voted_number)}
+            >
+              {i.voted_number}
+            </button>
+          );
+        })}
       </div>
     </div>
   );
